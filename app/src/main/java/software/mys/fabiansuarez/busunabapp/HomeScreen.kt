611006f6cf4dae3f83842534c84eefe37cf85164
Fabian Enrique Suarez Carvajal <inge.fabiansuarez@gmail.com>
2024-07-13 16:07:57 -0500

    Se ajusto el botom naigation.

--- app/src/main/java/software/mys/fabiansuarez/busunabapp/HomeScreen.kt
+++ app/src/main/java/software/mys/fabiansuarez/busunabapp/HomeScreen.kt
@@ -1,7 +1,9 @@
 package software.mys.fabiansuarez.busunabapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +17,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -23,11 +26,18 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -47,9 +57,23 @@ import software.mys.fabiansuarez.busunabapp.ui.theme.Typography
 fun HomeScreen() {
     BusUnabAppTheme {
         val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+        var selectedItem by remember { mutableIntStateOf(0) }
+        val items = listOf("Home", "Buses", "MyAccount")
 
         Scaffold(
             modifier = Modifier.fillMaxSize(),
+            bottomBar = {
+                NavigationBar {
+                    items.forEachIndexed { index, item ->
+                        NavigationBarItem(
+                            icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                            label = { Text(item) },
+                            selected = selectedItem == index,
+                            onClick = { selectedItem = index }
+                        )
+                    }
+                }
+            },
             topBar = {
                 MediumTopAppBar(
                     colors = TopAppBarDefaults.topAppBarColors(
@@ -97,18 +121,18 @@ fun BodyHomeScreen(modifier: Modifier) {
     Column(
         modifier
             .padding(16.dp)
-            .verticalScroll(rememberScrollState())
+
     ) {
         Card(
             elevation = CardDefaults.cardElevation(8.dp),
-            modifier = Modifier.height(400.dp)
+            modifier = Modifier.height(350.dp)
         ) {
             Text(text = stringResource(R.string.where_are_the_buses), Modifier.padding(8.dp))
             Map()
         }
         Spacer(modifier = Modifier.height(24.dp))
         Text(text = "Buses available", style = Typography.labelLarge)
-        LazyRow(modifier = Modifier.padding(top = 8.dp)) {
+        LazyColumn(modifier = Modifier.padding(top = 8.dp)) {
             items(fakeDataBus()) {
                 ItemBusAvailabel(it)
             }
@@ -121,31 +145,37 @@ fun BodyHomeScreen(modifier: Modifier) {
 fun ItemBusAvailabel(bus: Bus) {
     Card(
         modifier = Modifier
-            .fillMaxHeight()
-            .width(150.dp)
+            .fillMaxWidth()
+            .height(80.dp)
             .padding(
-                end = 8.dp
+                bottom = 8.dp
             )
     ) {
-        Column {
+        Row {
             AsyncImage(
+
                 model = bus.drier.urlImagePhoto,
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.height(150.dp)
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
             )
-            Text(
-                style = Typography.bodyLarge,
-                text = bus.drier.name,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
-            )
-            Text(
-                textAlign = TextAlign.Center,
-                text = bus.name,
-                style = Typography.bodyMedium,
-                modifier = Modifier.fillMaxWidth()
-            )
+            Column (Modifier.weight(2f).fillMaxHeight(), verticalArrangement = Arrangement.Center) {
+                Text(
+                    style = Typography.bodyLarge,
+                    text = bus.drier.name,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = bus.name,
+                    style = Typography.bodyMedium,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+
         }
     }
 }
